8d7ede7fa97f45e8bb5930fddb7265a63d12ece8
fanjiangxu <fanjiangxu@tidebuy.net>
2019-02-14 15:50:07 +0800

    add
    add

---
+++ FS.Currency/FS.Currency/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FS.Currency
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
